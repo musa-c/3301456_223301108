9d8b1e8471b20e25f2f4b30bbc8bdab05d6f22b8
Musa Civelek <musacivelekx@gmail.com>
2023-06-18 15:13:30 +0300

    directory changed

---
+++ lib/feature/login/log_in_view.dart
@@ -0,0 +1,273 @@
+// giriş yap
+
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'dart:convert';
+import 'package:abc/feature/login/sign_up_view.dart';
+import 'package:abc/product/controllers/concrete/user_controller.dart';
+import 'package:abc/product/widgets/app_title_widget.dart';
+import 'package:abc/product/widgets/button_widget.dart';
+import 'package:abc/product/widgets/text_field_widget.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import '../../../product/models/post_model.dart';
+import '../../product/constants/color_constants.dart';
+import '../../product/constants/string_constants.dart';
+import '../../tabbar_view.dart';
+
+class LogInView extends StatefulWidget {
+  const LogInView({super.key});
+
+  @override
+  State<LogInView> createState() => _LogInViewState();
+}
+
+class _LogInViewState extends State<LogInView> {
+  String? userOrEmail;
+  String? pass;
+  late bool sifregizle = true;
+  bool isEmailAuth = false;
+  bool isLoading = false;
+  bool isNull = false;
+
+  void userLogin(String userOrEmail, String password) async {
+    setState(() {
+      isLoading = true;
+    });
+    final http.Response response;
+    UserController userContoroller = UserController();
+    try {
+      if (isEmailAuth) {
+        response =
+            await userContoroller.emailorPasswordCheck(userOrEmail, password);
+      } else {
+        response = await userContoroller.usernameOrPasswordCheck(
+            userOrEmail, password);
+      }
+
+      if (response.statusCode == 200) {
+        setState(() {
+          isLoading = false;
+        });
+        // ignore: use_build_context_synchronously
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => TabBarViewAbc(),
+              settings: RouteSettings(
+                arguments: {"user": User.fromJson(json.decode(response.body))},
+              ),
+            ));
+
+        // return User.fromJson(jsonDecode(response.body));
+      } else if (response.statusCode == 500) {
+        // ignore: use_build_context_synchronously
+        showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: Text('Hata'),
+              content: Text('Hatalı giriş lütfen tekrar deneyiniz.'),
+              actions: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: Text('Tamam'),
+                ),
+              ],
+            );
+          },
+        );
+        setState(() {
+          isLoading = false;
+        });
+
+        // Handle other status codes if needed
+      } else {
+        setState(() {
+          isLoading = false;
+        });
+        print(response.statusCode);
+        print("başarısız");
+      }
+    } catch (e) {
+      setState(() {
+        isLoading = false;
+      });
+
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('Hata'),
+            content: Text('API isteği sırasında bir hata oluştu: $e'),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text('Tamam'),
+              ),
+            ],
+          );
+        },
+      );
+    }
+  }
+
+  bool checkValidation(String? userOrEmail, String? password) {
+    if (userOrEmail != null && password != null) {
+      setState(() {
+        isNull = false;
+      });
+      return true;
+    } else {
+      setState(() {
+        isNull = true;
+      });
+      return false;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      routes: {"signup": (context) => SignUpView()},
+      home: Scaffold(
+          resizeToAvoidBottomInset: false,
+          backgroundColor: ColorConstants.blackColor,
+          body: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              _appTitle,
+              isNull
+                  ? Text(
+                      isEmailAuth
+                          ? StringConstants.nullisEmailAuthTrue
+                          : StringConstants.nullisEmailAuthFalse,
+                      style: TextStyle(color: ColorConstants.errorRed),
+                    )
+                  : Container(),
+              SizedBox(height: 16),
+              _textFormKullAdi,
+              SizedBox(height: 16),
+              _textFormSifre,
+              SizedBox(height: 24),
+              _loginButton,
+              SizedBox(height: 16),
+              _info,
+              SizedBox(height: 36),
+              _orText,
+              SizedBox(height: 32),
+              _emailButton,
+              SizedBox(height: 24),
+              _googleButton,
+              SizedBox(height: 24),
+              _signUpButton
+            ],
+          )),
+    );
+  }
+
+  Widget get _appTitle => AppTitleWidget(app_title: StringConstants.appName);
+
+  Widget get _textFormKullAdi => TextFieldWidget(
+      onDataChanged: (text) {
+        setState(() {
+          userOrEmail = text;
+        });
+      },
+      hintText: isEmailAuth
+          ? StringConstants.isEmailAuthTruePlaceHolder
+          : StringConstants.isEmailAuthFalsePlaceHolder);
+
+  Widget get _textFormSifre => TextFieldWidget(
+      sifregizle: sifregizle,
+      iconButton: IconButton(
+        onPressed: () {
+          setState(() {
+            sifregizle = !sifregizle;
+          });
+        },
+        icon: sifregizle
+            ? Icon(Icons.visibility_off)
+            : Icon(Icons.remove_red_eye),
+        color: Colors.grey,
+      ),
+      onDataChanged: (text) {
+        setState(() {
+          pass = text;
+        });
+      },
+      hintText: "Şifre");
+
+  Widget get _loginButton => ButtonWidget(
+      onPressed: () {
+        if (checkValidation(userOrEmail, pass)) {
+          isLoading ? null : userLogin(userOrEmail!, pass!);
+        } else {
+          null;
+        }
+      },
+      child: isLoading ? CircularProgressIndicator() : Text("Giriş Yap"));
+
+  Widget get _info => SizedBox(
+        width: 380,
+        child: Text(
+          textAlign: TextAlign.center,
+          "verilerinizi felan çalıyoruz hiçbiri güvende değil haberiniz olsun",
+          style: TextStyle(
+            color: ColorConstants.greyColor,
+          ),
+        ),
+      );
+
+  Widget get _orText => Row(
+        children: [
+          _orTextLine,
+          Text(
+            "veya",
+            style: TextStyle(color: ColorConstants.greyColor),
+          ),
+          _orTextLine
+        ],
+      );
+
+  Widget get _orTextLine => Container(
+        width: 158,
+        height: 0.2,
+        color: Colors.grey,
+        margin: EdgeInsets.fromLTRB(16, 0, 16, 0),
+      );
+
+  Widget get _emailButton => ButtonWidget.Icon(Icon(Icons.person_2_outlined),
+      Text("Anonim Olarak Giriş Yap"), Colors.transparent, null,
+      borderSide: BorderSide(
+          style: BorderStyle.solid, color: ColorConstants.whiteColor));
+
+  Widget get _googleButton => ButtonWidget.Icon(
+          Icon(Icons.abc, color: ColorConstants.blackColor),
+          Text(
+              isEmailAuth
+                  ? StringConstants.isEmailAuthTrue
+                  : StringConstants.isEmailAuthFalse,
+              style: TextStyle(color: Colors.black)),
+          Colors.white, () {
+        setState(() {
+          isEmailAuth = !isEmailAuth;
+        });
+      });
+
+  Widget get _signUpButton => ButtonWidget.Icon(
+        Icon(Icons.person_add_alt_1_outlined),
+        Text(
+          "Yeni Hesap Oluştur",
+          style: TextStyle(color: ColorConstants.whiteColor),
+        ),
+        ColorConstants.blueColor,
+        () {
+          Navigator.pushNamed(context, 'signup');
+        },
+      );
+}
---
+++ lib/feature/login/sign_up_view.dart
@@ -0,0 +1,223 @@
+// kayıt ol
+
+// giriş yap
+
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:abc/product/controllers/concrete/user_controller.dart';
+import 'package:abc/product/widgets/app_title_widget.dart';
+import 'package:abc/product/widgets/button_widget.dart';
+import 'package:abc/product/widgets/text_field_widget.dart';
+import 'package:flutter/material.dart';
+
+class SignUpView extends StatefulWidget {
+  const SignUpView({super.key});
+
+  @override
+  State<SignUpView> createState() => _SignUpViewState();
+}
+
+class _SignUpViewState extends State<SignUpView> {
+  String? userName;
+  String? email;
+  String? password;
+  bool isDataAdded = false;
+  bool isLoading = false;
+  bool isNull = false;
+  // Simüle edilmiş bir post işlemi
+
+  bool checkValidation(String? userName, String? email, String? password) {
+    if (userName != null && email != null && password != null) {
+      setState(() {
+        isNull = false;
+      });
+      return true;
+    } else {
+      setState(() {
+        isNull = true;
+      });
+      return false;
+    }
+  }
+
+  void showModal(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Başarılı'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Icon(
+                Icons.check_circle,
+                color: Colors.green,
+                size: 50,
+              ),
+              SizedBox(height: 16),
+              Text('Hesap oluşturuldu.'),
+            ],
+          ),
+          actions: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('Kapat'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void createUser(String userName, String email, String password) async {
+    setState(() {
+      isLoading = true;
+    });
+    UserController userController = UserController();
+
+    try {
+      final response =
+          await userController.createUser(userName, email, password);
+
+      if (response.statusCode == 201) {
+        setState(() {
+          isLoading = false;
+          isDataAdded = true;
+        });
+
+        Future.delayed(Duration(seconds: 2), () {
+          setState(() {
+            isLoading = false;
+            showModal(context);
+          });
+        });
+
+        // return User.fromJson(jsonDecode(response.body));
+      } else if (response.statusCode == 500) {
+        // ignore: use_build_context_synchronously
+        showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: Text('Hata'),
+              content: Text(
+                  'e mail veya kullanıcı adı zaten kullanılıyor lütfen değiştirin.'),
+              actions: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: Text('Tamam'),
+                ),
+              ],
+            );
+          },
+        );
+        setState(() {
+          isLoading = false;
+        });
+
+        // Handle other status codes if needed
+      }
+    } catch (e) {
+      setState(() {
+        isLoading = false;
+      });
+
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('Hata'),
+            content: Text('API isteği sırasında bir hata oluştu: $e'),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text('Tamam'),
+              ),
+            ],
+          );
+        },
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        resizeToAvoidBottomInset: false,
+        backgroundColor: Colors.black,
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            _appTitle,
+            isNull
+                ? Text(
+                    "Lütfen alanların hepsini doldurunuz.",
+                    style: TextStyle(color: Colors.red),
+                  )
+                : Container(),
+            SizedBox(height: 16),
+            _textFormName,
+            SizedBox(height: 16),
+            _textFormKullAdi,
+            SizedBox(height: 16),
+            _textFormEmail,
+            SizedBox(height: 16),
+            _textFormSifre,
+            SizedBox(height: 24),
+            _loginButton,
+            SizedBox(height: 24),
+            _textButton,
+          ],
+        ));
+  }
+
+  Widget get _appTitle => AppTitleWidget(app_title: "PAYLAP");
+  Widget get _textFormName => TextFieldWidget(hintText: "İsim");
+  Widget get _textFormKullAdi => TextFieldWidget(
+      hintText: "Kullanıcı Adı",
+      onDataChanged: (value) => setState(() {
+            userName = value;
+          }));
+  Widget get _textFormEmail => TextFieldWidget(
+      hintText: "Email",
+      onDataChanged: (value) => setState(() {
+            email = value;
+          }));
+
+  Widget get _textFormSifre => TextFieldWidget(
+      hintText: "Şifre",
+      onDataChanged: (value) => setState(() {
+            password = value;
+          }),
+      iconButton: IconButton(
+        onPressed: () {},
+        icon: Icon(Icons.remove_red_eye),
+        color: Colors.grey,
+      ));
+
+  Widget get _loginButton => ButtonWidget(
+      onPressed: () {
+        if (checkValidation(userName, email, password)) {
+          isLoading ? null : createUser(userName!, email!, password!);
+        } else {
+          null;
+        }
+      },
+      child: isLoading ? CircularProgressIndicator() : Text("Kaydol"));
+
+  Widget get _textButton => ButtonWidget.textBtn(
+        () {
+          Navigator.pushNamed(context, 'login');
+        },
+        Text(
+          'Zaten bir hesabın var mı? \n Giriş Yap',
+          textAlign: TextAlign.center,
+        ),
+      );
+}
