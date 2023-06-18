941e7eb53c5f73c7df201687609350d4cc9d2994
Musa Civelek <musacivelekx@gmail.com>
2023-06-18 15:12:53 +0300

    api ulr constants string add

---
+++ lib/product/constants/api_url_constants.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+@immutable
+class ApiUrlConstants {
+  const ApiUrlConstants._();
+  static const String remoteUrl = "http://192.168.1.6:45455/";
+
+  static const String getAllUserUrl = "${remoteUrl}api/users";
+  static const String createUserUrl = "$getAllUserUrl/createUser";
+  static const String emailorPasswordCheckUrl =
+      "$getAllUserUrl/emailorPasswordCheck";
+  static const String usernameOrPasswordCheck =
+      "$getAllUserUrl/usernameOrPasswordCheck";
+
+  static const String getAllPostUrl = "${remoteUrl}api/posts";
+  static const String createPostUrl = "$getAllPostUrl/CreatePost";
+  static const String getPostByUserIdUrl = "$getAllPostUrl/GetPostByUserId";
+  static const String updatePostUrl = "$getAllPostUrl/updatePost";
+  static const String deletePostUrl = "$getAllPostUrl/delete";
+
+  static const String createLikeUrl = "${remoteUrl}api/likes/CreateLike";
+  static const String getUserLikesByPostIdUrl =
+      "${remoteUrl}api/likes/getUserLikesByPostId";
+}
