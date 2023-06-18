d3f2c15c9816449cfa3a8dd933b44df65954d3e1
Musa Civelek <musacivelekx@gmail.com>
2023-06-19 00:02:00 +0300

    icon animated add

--- lib/product/widgets/list_icon_widget.dart
+++ lib/product/widgets/list_icon_widget.dart
@@ -1,45 +1,123 @@
 import 'package:flutter/material.dart';
 
 class ListIconWidget extends StatefulWidget {
-  IconData icon;
-  Color? color;
-  int? count;
-  Function? onTap;
-  Function? onTapUser;
+  final IconData icon;
+  final Color? color;
+  final int? count;
+  final Function? onTap;
+  final Function? onTapUser;
+  final double? size;
 
-  ListIconWidget(
-      {required this.icon,
-      super.key,
-      this.color,
-      this.count,
-      this.onTap,
-      this.onTapUser});
+  const ListIconWidget({
+    super.key,
+    required this.icon,
+    this.color,
+    this.count,
+    this.onTap,
+    this.onTapUser,
+    this.size,
+  });
 
   @override
   State<ListIconWidget> createState() => ListIconWidgetState();
 }
 
-class ListIconWidgetState extends State<ListIconWidget> {
+class ListIconWidgetState extends State<ListIconWidget>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<double> _animation;
+  final double _iconSize = 14;
+
   @override
-  Widget build(BuildContext context) {
-    return Wrap(spacing: 5, children: [
-      InkWell(
-        onTap: () => widget.onTap!() ?? () {},
-        child: Icon(
-          widget.icon,
-          color: widget.color ?? _footerColor,
-          size: 14,
-        ),
-      ),
-      InkWell(
-        onTap: () => widget.onTapUser!() ?? () {},
-        child: _listIconText,
-      )
-    ]);
+  void initState() {
+    super.initState();
+
+    _animationController = AnimationController(
+        duration: const Duration(milliseconds: 500), vsync: this
+        // sayfanın dikey senkronizayonunu sağlanması için vysnc: this.
+
+        /* 
+      Animasyonları kontrol etmek için kullanılan bir sınıftır. 
+      Bu durumda, animasyonun süresi 1000 milisaniye (1 saniye) olarak ayarlanmıştır.
+      */
+        );
+
+    _animation = Tween<double>(begin: 1, end: 1.5).animate(
+      CurvedAnimation(parent: _animationController, curve: Curves.ease),
+      /* 
+       Bu, animasyonun boyutunu değiştirmek için kullanılan bir faktördür.
+      */
+    );
+
+    _animationController.addStatusListener((status) {
+      // durum dinleyicisi animasyon tamamlandıysa tersine çevririr. böylece animasyon tekrarlanır.
+      if (status == AnimationStatus.completed) {
+        _animationController.reverse();
+      }
+    });
   }
 
-  Color get _footerColor => const Color.fromRGBO(85, 90, 100, 1.0);
+  // initin tam tersinidir. state ile ilişki sona erildiğinde çalışır.
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
 
-  Widget get _listIconText =>
-      Text(widget.count.toString(), style: TextStyle(color: _footerColor));
+  void _toggleAnimation() {
+    if (_animationController.isAnimating) {
+      return;
+    }
+
+    // setState(() {
+    //   _iconSize =
+    //       14; // İlk değere geri dönmesi için _iconSize'ı 14 olarak ayarlayın
+    // });
+
+    // animasyonu başlatır.
+    _animationController.forward();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Wrap(
+      spacing: 5,
+      children: [
+        AnimatedBuilder(
+          animation: _animationController,
+          builder: (context, child) {
+            return InkWell(
+              onTap: () {
+                _toggleAnimation();
+                widget.onTap!() ?? () {};
+              },
+              child: Transform.scale(
+                scale: _animation.value,
+                child: Icon(
+                  widget.icon,
+                  color: widget.color ?? _getFooterColor(),
+                  size: _iconSize,
+                ),
+              ),
+            );
+          },
+        ),
+        InkWell(
+          onTap: widget.onTapUser as void Function()?,
+          child: _buildListIconText(),
+        ),
+      ],
+    );
+  }
+
+  Color _getFooterColor() {
+    return const Color.fromRGBO(85, 90, 100, 1.0);
+  }
+
+  Widget _buildListIconText() {
+    return Text(
+      widget.count.toString(),
+      style: TextStyle(color: _getFooterColor()),
+    );
+  }
 }
